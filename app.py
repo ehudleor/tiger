cacaa06db49542817fa6ba175957c5f53c247acf
ehudleor <118942950+ehudleor@users.noreply.github.com>
2022-12-08 18:39:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -135,3 +135,6 @@ if app_mode == 'Segment an Image':
     # choosing a k value (either with +- or with a slider)
     k_value = st.sidebar.number_input('Insert K value (number of clusters):', value=4, min_value = 1) # asks for input from the user
     st.sidebar.markdown('---') # adds a devider (a line)
+    
+     attempts_value_slider = st.sidebar.slider('Number of attempts', value = 7, min_value = 1, max_value = 10) # slider example
+    st.sidebar.markdown('---') # adds a devider (a line)
