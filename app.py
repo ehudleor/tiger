777b0711b12fcd99e768ce973d37cea2addfa66f
ehudleor <118942950+ehudleor@users.noreply.github.com>
2022-12-08 18:08:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-qwqwqw
+# import libs
+import streamlit as st
+import cv2
+import numpy as np
+import skimage.io as io
+#import matplotlib.pyplot as plt
+
+# check versions
+#np.__version__
+
+################################# function to segment using k-means
+
+# vars
+DEMO_IMAGE = 'demo.png' # a demo image for the segmentation page, if none is uploaded
+favicon = 'favicon.png'
