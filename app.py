0fdb6c517e0a1a8e9170a2c378910b1e9e5f51e9
ehudleor <118942950+ehudleor@users.noreply.github.com>
2022-12-11 00:23:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def otsu_process(imag):
   ##img = io.imread(file_path)
   ##img_gray = io.imread(file_path, as_gray=True)
   masked_image_grayscale = rgb2gray(imag)
-  thresholds = threshold_multiotsu(masked_image_grayscale, classes=3)  #find the values of the thresholds
+  thresholds = threshold_multiotsu(masked_image_grayscale, classes=4)  #find the values of the thresholds
   regions = np.digitize(masked_image_grayscale, bins=thresholds)
   #plt.imshow(regions, cmap = 'Greens_r')
   return regions
